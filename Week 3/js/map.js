5dd0f55b3f482c9f421cc8e7bd01c482e16504db
kaicolorado <kaicolorado6@gmail.com>
2021-04-18 22:03:47 -0700

    story map

--- Week 3/js/map.js
+++ Week 3/js/map.js
@@ -7,27 +7,32 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 let data = [
    {
-	'title':'UCLA: Where I currently live',
+	'title':'UCLA',
+	'desc' : 'Where I currently live',
    	'lat': 34.0679842,
     'lon': -118.4491570
   },  
 	    {
-		    'title':'My House in San Diego: Where my parents currently live',
-		    'lat': 32.8343872,
+		    'title':'San Diego',
+		    'desc' : 'Where I grew up and where my parents currently live',
+			'lat': 32.8343872,
 		    'lon': -117.0900217
 	    },
     	{
-		    'title':'Santa Rosa: Where my dad grew up',
+		    'title':'Santa Rosa',
+			'desc' : 'Where my dad grew up',
     		'lat': 38.4507486,
 	    	'lon': -122.7478461
 	    },
     	{
-	    	'title':'San Bruno: Where my mom grew up',
+	    	'title':'San Bruno',
+			'desc' : 'Where my mom grew up',
     		'lat': 37.6206578,
 	    	'lon': -122.4194064
 	    },
     	{
-    		'title':'SDSU: Where my parents met',
+    		'title':'SDSU',
+			'desc' : 'Where my parents went to college',
 	    	'lat': 32.7746470,
 		    'lon': -117.0715014
 	    }
@@ -38,7 +43,8 @@ let data = [
 
         data.forEach(function(item, index){
             var marker = L.marker([item.lat,item.lon])
-				.bindPopup(item.title)
+				.bindPopup(item.desc)
+				
 				
 
 				myMarkers.addLayer(marker);
